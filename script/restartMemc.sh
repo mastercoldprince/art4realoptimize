edbfbbb75367e83c87cc0269b9509f76be7ce516
lokiagg <1306426768@qq.com>
2024-04-21 14:34:15 +0800

    basic configuration changes

--- script/restartMemc.sh
+++ script/restartMemc.sh
@@ -4,10 +4,10 @@ addr=$(head -1 ../memcached.conf)
 port=$(awk 'NR==2{print}' ../memcached.conf)
 
 # kill old me
-ssh ${addr} -o StrictHostKeyChecking=no "cat /tmp/memcached.pid | xargs kill"
+ssh ${addr} -o StrictHostKeyChecking=no "cat /var/run/memcached/memcached.pid | xargs kill"
 
 # launch memcached
-ssh ${addr} -o StrictHostKeyChecking=no "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /tmp/memcached.pid"
+ssh ${addr} -o StrictHostKeyChecking=no "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /var/run/memcached/memcached.pid"
 sleep 1
 
 # init 
