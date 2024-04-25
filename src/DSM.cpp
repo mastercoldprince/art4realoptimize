c3e4bd9fad0fb15fa423b9ec894cc108de9c21a7
lokiagg <1306426768@qq.com>
2024-04-25 14:18:18 +0800

    [test]
    查看每次读写大小

--- src/DSM.cpp
+++ src/DSM.cpp
@@ -143,7 +143,7 @@ void DSM::read(char *buffer, GlobalAddress gaddr, size_t size, bool signal,
              ctx->coro_id);
     (*ctx->yield)(*ctx->master);
   }
-    printf("write size : %" PRIu64 " \n",size);
+    printf("read size : %" PRIu64 " \n",size);
 }
 
 void DSM::read_sync(char *buffer, GlobalAddress gaddr, size_t size,
