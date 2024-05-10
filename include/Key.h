2d225e01b7e66441fbcd57ed5c8dc8e4d0caaa9d
lokiagg <1306426768@qq.com>
2024-05-10 22:33:04 +0800

    [debug]
    改一些小bug

--- include/Key.h
+++ include/Key.h
@@ -141,7 +141,7 @@ inline Key int2key(uint64_t key) {
   }
   for (int i = 1; i <=(int) keylen; ++ i) {
     auto shr = (keylen - i) * 8;
-    res.at(i - 1) = (shr >= 64u ? 0 : ((key >> (uint64_t)shr) & ((1 << 8) - 1))); // Is equivalent to padding zero for short key
+    res.at(i - 1) = ((uint64_t)shr >= 64u ? 0 : ((key >> (uint64_t)shr) & ((1 << 8) - 1))); // Is equivalent to padding zero for short key
   }
   res.at(64)=keylen;
   return res;
