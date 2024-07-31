229fc4ef90702f616fd50d51b1caa1c62d41a2cf
lokiagg <1306426768@qq.com>
2024-08-01 00:18:28 +0800

    [debug]

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -684,7 +684,8 @@ void Tree::insert(const Key &k, Value v, CoroContext *cxt, int coro_id, bool is_
   else {leaf_type += 4;leaf_size += 1024;}
   }
 
-  printf("thread %d insert kv: %d\n" ,(int)dsm->getMyThreadID( ),(int)key2int(k));
+  printf("thread %d insert kv: %d\n" ,(int)dsm->getMyThreadID( ),);
+  if((int)key2int(k) == 5308670) goto insert_finish;
   // traversal
   GlobalAddress p_ptr;
   InternalEntry p;
