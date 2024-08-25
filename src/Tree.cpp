a9b4460a2886a8e87a0a3152d1611c4b50474698
lokiagg <1306426768@qq.com>
2024-08-25 21:07:45 +0800

    Update Tree.cpp

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -982,9 +982,9 @@ if(parent_type ==0)  //一个内部节点    1.继续往下找  2. 有一个空�
             auto cas_node_type_buffer = (dsm->get_rbuf(coro_id)).get_cas_buffer();
             InternalEntry new_entry(p);
             new_entry.child_type = 2;
-            new_entry.node_type = 7;
+            new_entry.node_type = node_type_to_num(NODE_256);
             new (hdr_buffer) Header(bhdr); 
-            new (cas_node_type_buffer) InternalEntry(p);
+            new (cas_node_type_buffer) InternalEntry(new_entry);
 
             
             RdmaOpRegion *rs =  new RdmaOpRegion[2];
@@ -1000,7 +1000,7 @@ if(parent_type ==0)  //一个内部节点    1.继续往下找  2. 有一个空�
            // dsm->cas(GADD(p.addr(), sizeof(GlobalAddress)), (uint64_t)bhdr, (uint64_t)new_hdr, hdr_buffer, sizeof(Header), false, cxt);
            // dsm->cas(p_ptr,(uint64_t)p,(uint64_t)new_entry,cas_node_type_buffer,sizeof(InternalEntry), false, cxt);
            //需要同步吗？？？？？？？？？  
-            dsm->two_cas_mask(rs[0],(uint64_t)bhdr,(uint64_t)hdr_buffer,1UL << (64 - 1) ,rs[1],(uint64_t)p,(uint64_t)cas_node_type_buffer,1UL << (64 -1),false,cxt);
+            dsm->two_cas_mask(rs[0],(uint64_t)bhdr,(uint64_t)hdr_buffer,~0UL ,rs[1],(uint64_t)p,(uint64_t)cas_node_type_buffer,~0UL,false,cxt);
             index_cache->change_node_type(entry_ptr);
             goto next;
           }
@@ -1327,7 +1327,7 @@ else{  //一个缓冲节点 1.找到一样的叶节点了 2.插空槽 3.缓冲�
             auto cas_node_type_buffer = (dsm->get_rbuf(coro_id)).get_cas_buffer();
             BufferEntry new_entry(bp);            
             new_entry.node_type = 2;
-            new_entry.leaf_type = 7;
+            new_entry.leaf_type = node_type_to_num(NODE_256);
             new (hdr_buffer) Header(bhdr);
             new (cas_node_type_buffer) BufferEntry(new_entry);
             
@@ -1344,7 +1344,7 @@ else{  //一个缓冲节点 1.找到一样的叶节点了 2.插空槽 3.缓冲�
            // dsm->cas(GADD(p.addr(), sizeof(GlobalAddress)), (uint64_t)bhdr, (uint64_t)new_hdr, hdr_buffer, sizeof(Header), false, cxt);
            // dsm->cas(p_ptr,(uint64_t)p,(uint64_t)new_entry,cas_node_type_buffer,sizeof(InternalEntry), false, cxt);
            //需要同步吗？？？？？？？？？  
-            dsm->two_cas_mask(rs[0],(uint64_t)bhdr,(uint64_t)hdr_buffer,1UL << (64 - 1) ,rs[1],(uint64_t)bp,(uint64_t)cas_node_type_buffer,1UL <<( 64 -1),false,cxt);
+            dsm->two_cas_mask(rs[0],(uint64_t)bhdr,(uint64_t)hdr_buffer,~0UL ,rs[1],(uint64_t)bp,(uint64_t)cas_node_type_buffer,~0UL,false,cxt);
             goto next;
           }
           else{  //有重复的 需要将重复的拿下来到下一级缓冲节点
