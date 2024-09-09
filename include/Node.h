2755d4744fa7ce498d2d5df8f27de9dcb56804f8
lokiagg <1306426768@qq.com>
2024-09-09 15:13:53 +0800

    debug

--- include/Node.h
+++ include/Node.h
@@ -986,7 +986,7 @@ public:
   bool is_valid(const GlobalAddress& p_ptr, int depth, bool from_cache) const { return hdr.type() != NODE_DELETED && hdr.depth <= depth && (!from_cache || p_ptr == rev_ptr); }
 } __attribute__((packed));
 
-
+ 
 //static_assert(sizeof(InternalPage) == 8 + 8 + 256 * 8);
 
 
