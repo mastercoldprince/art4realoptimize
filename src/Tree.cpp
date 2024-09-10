54bbaa879dfd1715867241dc583f3fde861c6be0
lokiagg <1306426768@qq.com>
2024-09-10 19:45:21 +0800

    Update Tree.cpp

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -1559,13 +1559,13 @@ bool Tree::out_of_place_write_buffer_node(const Key &k, Value &v, int depth,Inte
       {
         bnodes_entry_index[new_bnode_num - 1][j+1] = count_index[i][j+1];
         leaf_addrs[new_bnode_num - 1][j].val = bnode.records[count_index[i][j + 1]].val;
-        if(j > 0 )  bnode.records[count_index[i][j + 1]] = BufferEntry::Null();
         RdmaOpRegion r;
         r.dest       = bnode.records[count_index[i][j + 1]].addr();
         assert(r.dest !=0);
         r.size       = sizeof(Leaf_kv);
         r.is_on_chip = false;
         rs.push_back(r);
+        if(j > 0 )  bnode.records[count_index[i][j + 1]] = BufferEntry::Null();
       }
     }
   }
