9c0243135740e1e47a7660ae31150db2bfad786e
lokiagg <1306426768@qq.com>
2024-09-10 15:38:47 +0800

    Update Node.h

--- include/Node.h
+++ include/Node.h
@@ -914,13 +914,25 @@ public:
   };
 
 public:
-  BufferEntry() : val(0) {}
+  BufferEntry() : val(0) 
+      {
+        
+      }
   BufferEntry(uint8_t node_type, uint8_t partial,uint8_t prefix_type,uint8_t leaf_type,const GlobalAddress &addr) :
-                partial(partial),node_type(node_type), prefix_type(prefix_type),leaf_type(leaf_type),packed_addr{addr.nodeID, addr.offset >> ALLOC_ALLIGN_BIT} {}
+                partial(partial),node_type(node_type), prefix_type(prefix_type),leaf_type(leaf_type),packed_addr{addr.nodeID, addr.offset >> ALLOC_ALLIGN_BIT}
+                 {
+
+                 }
   BufferEntry(const BufferEntry& e) :
-                partial(e.partial),node_type(e.node_type), prefix_type(e.prefix_type), leaf_type(e.leaf_type),packed_addr(e.packed_addr) {}
+                partial(e.partial),node_type(e.node_type), prefix_type(e.prefix_type), leaf_type(e.leaf_type),packed_addr(e.packed_addr) 
+                {
+
+                }
   BufferEntry(NodeType node_type, const BufferEntry& e) :
-                partial(e.partial),node_type(e.node_type),prefix_type(e.prefix_type),leaf_type(static_cast<uint8_t>(node_type)),packed_addr(e.packed_addr) {}
+                partial(e.partial),node_type(e.node_type),prefix_type(e.prefix_type),leaf_type(static_cast<uint8_t>(node_type)),packed_addr(e.packed_addr)
+                 {
+
+                 }
   operator uint64_t() const { return val; }
 
   static BufferEntry Null() {
