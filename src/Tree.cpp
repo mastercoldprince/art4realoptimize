95fa6258a79e2fdc8e7aaf7bfff2558c2d19e0e2
lokiagg <1306426768@qq.com>
2024-09-19 00:04:10 +0800

    search

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -2256,7 +2256,6 @@ bool Tree::search(const Key &k, Value &v, CoroContext *cxt, int coro_id) {   ///
     assert(entry_idx >= 0);
     p_ptr = GADD(entry_ptr->addr, sizeof(InternalEntry) * entry_idx);
     p = entry_ptr->records[entry_idx];
-    node_ptr = entry_ptr->addr;
     depth = entry_ptr->depth;
   }
   else {
