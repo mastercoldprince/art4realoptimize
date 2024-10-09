5e947c8dfa3ce52938e40b4b90d15c17bfea467c
lokiagg <1306426768@qq.com>
2024-10-09 20:48:11 +0800

    debug

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -1649,7 +1649,7 @@ bool Tree::out_of_place_write_buffer_node(const Key &k, Value &v, int depth,Inte
     bnode->records[bnodes_entry_index[i][1]].packed_addr={bnode_addrs[i].nodeID, bnode_addrs[i].offset >> ALLOC_ALLIGN_BIT};
     bnode->records[bnodes_entry_index[i][1]].node_type = 1;
    // printf("thread  %d 15 node value is %" PRIu64" \n",(int)dsm->getMyThreadID( ),(uint64_t)(new_bnodes[i]->hdr));
-   assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.mn_id == 0);
+  // assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.mn_id == 0);
    assert(new_bnodes[i]->hdr.val != 0);
   }
 
@@ -1854,7 +1854,7 @@ bool Tree::out_of_place_write_buffer_node_from_buffer(const Key &k, Value &v, in
     for(int j =0;j<bnodes_entry_index[i][0];j++)
     {
       new_bnodes[i]->records[j].val = leaf_addrs[i][j].val;
-      assert(new_bnodes[i]->records[j].packed_addr.mn_id == 0);
+    //  assert(new_bnodes[i]->records[j].packed_addr.mn_id == 0);
       leaf_key.push_back(leaves[leaf_cnt].get_key());
       leaf_cnt ++;
     }
@@ -1884,7 +1884,7 @@ bool Tree::out_of_place_write_buffer_node_from_buffer(const Key &k, Value &v, in
     bnode->records[bnodes_entry_index[i][1]].packed_addr={bnode_addrs[i].nodeID, bnode_addrs[i].offset >> ALLOC_ALLIGN_BIT};
     bnode->records[bnodes_entry_index[i][1]].node_type = 1;
    // printf("thread  %d 15 node value is %" PRIu64" \n",(int)dsm->getMyThreadID( ),(uint64_t)(new_bnodes[i]->hdr));
-   assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.mn_id == 0);
+//   assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.mn_id == 0);
    assert(new_bnodes[i]->hdr.val != 0);
   }
 
