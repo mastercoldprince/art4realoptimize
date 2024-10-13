b5c2dbcfab58c7209bc8cf3696cde27c10830ddf
lokiagg <1306426768@qq.com>
2024-10-13 23:10:13 +0800

    Update Tree.cpp

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -2392,7 +2392,7 @@ bool Tree::out_of_place_write_buffer_node(const Key &k, Value &v, int depth,Inte
     bnode->records[bnodes_entry_index[i][1]].packed_addr={bnode_addrs[i].nodeID, bnode_addrs[i].offset >> ALLOC_ALLIGN_BIT};
     bnode->records[bnodes_entry_index[i][1]].node_type = 1;
     bnode->records[bnodes_entry_index[i][1]].prefix_type = 1;
-    assert(new_bnodes[i]->records[bnodes_entry_index[i][0]].packed_addr.offset !=0);
+    assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.offset !=0);
    // printf("thread  %d 15 node value is %" PRIu64" \n",(int)dsm->getMyThreadID( ),(uint64_t)(new_bnodes[i]->hdr));
   // assert(bnode->records[bnodes_entry_index[i][1]].packed_addr.mn_id == 0);
    assert(new_bnodes[i]->hdr.val != 0);
