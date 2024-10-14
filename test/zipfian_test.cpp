f527ea7102ba94e9b4e3ce35598cca42e73dd051
lokiagg <1306426768@qq.com>
2024-10-14 13:21:21 +0800

    Update zipfian_test.cpp

--- test/zipfian_test.cpp
+++ test/zipfian_test.cpp
@@ -316,7 +316,7 @@ printf("Cache \n");
 #else 
 printf("No cache\n");
 #endif
-    sleep(0.8);
+    sleep(2);
     clock_gettime(CLOCK_REALTIME, &e);
     int microseconds = (e.tv_sec - s.tv_sec) * 1000000 +
                        (double)(e.tv_nsec - s.tv_nsec) / 1000;
