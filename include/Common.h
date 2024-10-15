dd9335b22a94eaf259953f98b9bfeba718f60eb1
lokiagg <1306426768@qq.com>
2024-10-15 22:14:25 +0800

    Update Common.h

--- include/Common.h
+++ include/Common.h
@@ -94,7 +94,7 @@ constexpr uint64_t GB = 1024ull * MB;
 constexpr uint16_t kCacheLineSize = 64;
 
 // Remote Allocation
-constexpr uint64_t dsmSize           =100;        // GB  [CONFIG]
+constexpr uint64_t dsmSize           =80;        // GB  [CONFIG]
 constexpr uint64_t kChunkSize        = 16 * MB;   // B
 
 // Rdma Buffer
